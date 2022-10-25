078aae7009f90a69616f2aa4e2cb50b0a5417ab6
cjvuoksi <cjvuoksi@gmail.com>
2022-10-25 17:12:33 +0000

    added file

---
+++ src/elements/footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return(
+        <div>
+            <a href="https://github.com/cjvuoksi/creative3">Git Repo</a>
+        </div>
+    )
+}
+
+export default Footer
