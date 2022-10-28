7bd2595b26c384fe3b251298344d867f3d9a25c1
cjvuoksi <cjvuoksi@gmail.com>
2022-10-28 02:19:53 +0000

    content to about page, minor fixes

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,8 +3,17 @@ import React from 'react';
 //TODO Decide on this page
 function About() {
   return (
-    <div>
+    <div className="home">
       <h1>About</h1>
+      <p>Books are great. Especially when they are free. Search this catalog to find links 
+      to some great books in the public domain</p>
+      <p>Click on the links below each book to navigate to its entry on project 
+      Gutenberg which hosts the books </p> 
+      <p><i>“A reader lives a thousand lives before he dies . . . The man who never reads lives only one.”</i><br></br>
+      -George R.R. Martin</p>
+      <p><i>“Show me a family of readers, and I will show you the people who move the world.”</i><br></br>
+      -Napoléon Bonaparte
+      </p>
     </div>
   );
 };
