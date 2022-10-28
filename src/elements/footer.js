6e49db2a2f6f865f751b9cab1042286d3db1e150
cjvuoksi <cjvuoksi@gmail.com>
2022-10-28 02:22:24 +0000

    made git repo link open in a new tab

--- src/elements/footer.js
+++ src/elements/footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
     return(
         <div>
-            <a href="https://github.com/cjvuoksi/creative3">Git Repo</a>
+            <a href="https://github.com/cjvuoksi/creative3" target="_blank" rel="noopener noreferrer">Git Repo</a>
         </div>
     )
 }
